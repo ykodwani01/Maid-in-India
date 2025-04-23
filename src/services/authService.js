d5b454ec62136f2df3706b871b919a9ea4f9a6f1
ykodwani01 <yashkodwani2003@gmail.com>
2025-04-23 18:04:21 +0530

    FiXed user Bugs

--- src/services/authService.js
+++ src/services/authService.js
@@ -49,7 +49,7 @@ const updateProfile = async(uid,data) => {
   user.address = address || user.address ;
   user.profileCreated=true;
   await user.save();
-  return { name: user.name, contactNumber: user.contactNumber, address: user.address, profileCreated };
+  return { name: user.name, contactNumber: user.contactNumber, address: user.address, user.profileCreated };
 };
 
 const verifyGoogle = async (data) => {
